193066746e6be579f6781b4e038d906e40593a3d
exigibil <adrian.topa04@gmail.com>
2024-01-19 19:39:53 +0200

    ex1 finish

--- src/js/01-gallery.js
+++ src/js/01-gallery.js
@@ -1,5 +1,28 @@
 // Add imports above this line
 import { galleryItems } from './gallery-items';
+import simpleLightbox from 'simplelightbox';
 // Change code below this line
+// Descris în documentație
+import SimpleLightbox from "simplelightbox";
+// Import suplimentar de stil
+import "simplelightbox/dist/simple-lightbox.min.css";
 
-console.log(galleryItems);
+const galleryContainer = document.querySelector(".gallery");
+const createGalleryItems = galleryItems
+.map(({ original, preview, description }) => 
+`<li class="gallery__item"> 
+    <a class="gallery__link" href="${original}">
+      <img class="gallery__image" src="${preview}" data-src="${original}" alt="${description}" />
+    </a>
+</li>`
+).join("");
+
+
+galleryContainer.insertAdjacentHTML('beforeend', createGalleryItems);
+
+const galleryHandler = new simpleLightbox('.gallery a', {
+    captionsData: 'alt', 
+    captionDelay: 250
+});
+galleryHandler.on("show.simplelightbox");
+
--- src/js/02-video.js
+++ src/js/02-video.js
@@ -1,1 +1,28 @@
+import throttle from 'lodash.throttle';
 
+
+
+const vimeoPlayer = document.getElementById('vimeo-player');
+const player = new Vimeo.Player(document.getElementById('vimeo-player'));
+
+const savePlaybackTime = (time) => {
+    localStorage.setItem('videoplayer-current-time', time);
+};
+
+const handleTimeUpdate = throttle((data) => {
+    const currentTime = data.seconds;
+    savePlaybackTime(currentTime);
+}, 1000);
+
+player.on('timeupdate', handleTimeUpdate);
+
+const setPlaybackTimeFromStorage = () => {
+    const storedTime = localStorage.getItem('videoplayer-current-time')
+    if (storedTime !== null) {
+        const currentTime = parseFloat(storedTime);
+    }
+};
+
+player.ready().then(() => {
+    setPlaybackTimeFromStorage();
+})
